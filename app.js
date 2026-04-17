b7088b5a182e976aa8da216623fdd5dc6a20d55c
asawariku <124157262+asawariku@users.noreply.github.com>
2026-04-17 10:18:04 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const config = require('./config');
+
+console.log("App started...");
+console.log("API Key:", config.API_KEY);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    AWS_ACCESS_KEY_ID: "AKIAIOSFODNN7EXAMPLE",
+    AWS_SECRET_ACCESS_KEY: "abcd1234secretkeydemo",
+    API_KEY: "ghp_abcdEFGHijklMNOPqrstUVWXyz1234567890",
+    PASSWORD: "MySuperSecretPassword123"
+};
